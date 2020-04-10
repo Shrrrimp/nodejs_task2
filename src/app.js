885cac364a48059daa884cc3176cff5edeca866a
julia tokmakova <julia_tokmakova00@mail.ru>
2020-04-10 20:03:28 +0300

    feat: add middleware which log incoming requests

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+const { logger } = require('./helpers/logger');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -18,6 +19,16 @@ app.use('/', (req, res, next) => {
     res.send('Service is running!');
     return;
   }
+  next();
+});
+
+app.use('*', (req, res, next) => {
+  const { originalUrl, method, params, body } = req;
+  const message = `url: ${originalUrl}, method: ${method}, params: ${JSON.stringify(
+    params
+  )}, body: ${JSON.stringify(body)}`;
+
+  logger.log('info', message);
   next();
 });
 
